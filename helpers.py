f7b1ea4eebd769a5d73b721db88d43dcd539ee64
vGebs <vaughn.geber33@gmail.com>
2023-05-31 17:20:10 -0600

    Move helper functions to their own file

---
+++ helpers.py
@@ -0,0 +1,27 @@
+import openai
+import json
+
+
+def create_chat_model_prompt(content: str) -> dict:
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": content},
+        ],
+    )
+
+
+def parse_response_content(response):
+    content = response.choices[0].message["content"]
+    return json.loads(content)
+
+
+def remove_newlines(obj):
+    if isinstance(obj, str):
+        return obj.replace("\n", " ")
+    elif isinstance(obj, list):
+        return [remove_newlines(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: remove_newlines(value) for key, value in obj.items()}
+    else:
+        return obj
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 
 from dotenv import load_dotenv
+from helpers import create_chat_model_prompt, parse_response_content, remove_newlines
 import os
 import openai
 import json
@@ -10,31 +11,6 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
-
-
-def create_chat_model_prompt(content: str) -> dict:
-    return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": content},
-        ],
-    )
-
-
-def parse_response_content(response):
-    content = response.choices[0].message["content"]
-    return json.loads(content)
-
-
-def remove_newlines(obj):
-    if isinstance(obj, str):
-        return obj.replace("\n", " ")
-    elif isinstance(obj, list):
-        return [remove_newlines(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {key: remove_newlines(value) for key, value in obj.items()}
-    else:
-        return obj
 
 
 @app.route("/getWeeklyContent", methods=["POST"])
