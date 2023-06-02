51507b761967e3f0c9794196ab8821bd66ec8f82
vGebs <vaughn.geber33@gmail.com>
2023-06-01 18:27:15 -0600

    Add quiz routes to server.py

--- helpers.py
+++ helpers.py
@@ -20,14 +20,3 @@ def parse_response_content(response):
     content = response.choices[0].message["content"]
     gucci = remove_control_characters(content)
     return json.loads(gucci)
-
-
-# def remove_newlines(obj):
-#     if isinstance(obj, str):
-#         return obj.replace("\n", " ")
-#     elif isinstance(obj, list):
-#         return [remove_newlines(item) for item in obj]
-#     elif isinstance(obj, dict):
-#         return {key: remove_newlines(value) for key, value in obj.items()}
-#     else:
-#         return obj
--- server.py
+++ server.py
@@ -11,12 +11,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 from routes.courseCreationRoutes import bp as courseCreation_bp
 from routes.courseDefinitionRoutes import bp as courseDef_bp
 from routes.assignmentCreationRoutes import bp as assignmentCreation_bp
+from routes.quizCreationRoutes import bp as quizCreation_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(courseCreation_bp)
 app.register_blueprint(courseDef_bp)
 app.register_blueprint(assignmentCreation_bp)
+app.register_blueprint(quizCreation_bp)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
