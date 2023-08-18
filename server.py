b292094a1be4c0eaf365c884d1a3034db4123de3
vGebs <vaughn.geber33@gmail.com>
2023-08-18 16:59:41 -0600

    RE: Issue #11 -> Add firestore to project

--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 from flask import Flask
 
 from dotenv import load_dotenv
+import json
 import os
 import openai
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials, initialize_app
 
 # Set up OpenAI API
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Load your service account key JSON string from the environment variable
+firebase_service_key = os.getenv("FIREBASE_SERVICE_KEY")
+service_account_key = json.loads(firebase_service_key)
+
+# Initialize the Firebase Admin SDK with the service account key
+cred = credentials.Certificate(service_account_key)
+initialize_app(cred)
+
+# Now you can use Firestore and other Firebase services
+db = firestore.client()
 
 from routes.courseCreationRoutes import bp as courseCreation_bp
 from routes.courseDefinitionRoutes import bp as courseDef_bp
